eeca4f378d187a3d3eb63a9af359812038b37116
MaximeKELI <presidentnetero01@gmail.com>
2025-12-23 16:31:53 +0000

    Store

--- boutique_app/apps.py
+++ boutique_app/apps.py
@@ -5,4 +5,6 @@ class BoutiqueAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'boutique_app'
     verbose_name = "La Gloire de Dieu - Boutique"
-
+    
+    def ready(self):
+        import boutique_app.signals  # Import des signaux
